66bd8484a79644c5ba1d66cf9ffe5c61e30eacfc
Rikki Schulte <rikki.schulte@gmail.com>
2020-02-28 13:16:06 -0500

    feat: set up monaco service

--- packages/graphql-languageservice/src/index.ts
+++ packages/graphql-languageservice/src/index.ts
@@ -1,3 +1,2 @@
-export * from 'graphql-language-service-interface';
-export * from 'graphql-language-service-parser';
-export * from 'graphql-language-service-types';
+export * from 'graphql-language-service-types'
+export * from 'graphql-language-service-interface'
